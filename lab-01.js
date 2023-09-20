af94e20926ac01893d6303eb41f9117b670b2297
KuroiRyu1 <minhphat1612@gmail.com>
2023-09-20 19:28:02 +0700

    learn

---
+++ lab-01.js
@@ -0,0 +1,29 @@
+//Bai 1
+
+// cau a
+console.log('Hello World')
+// cau b
+console.log('Vo Nguyen Minh Phat')
+// cau c
+console.clear()
+// cau d
+console.log('*****')
+console.log('*****')
+console.log('*****')
+console.log('*****')
+console.log('*****')
+// cau e
+console.log('     *')
+console.log('    ***')
+console.log('   *****')
+console.log('  *******')
+console.log(' *********')
+console.log('***********')
+// cau f
+console.log(" ** ** ")
+console.log('*  *  *')
+console.log('*     *')
+console.log(' *   *')
+console.log('  * *')
+console.log('   *')
+
