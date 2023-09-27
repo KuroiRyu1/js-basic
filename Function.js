7e46a59513e2f5e9933b02ccc7f1ba3f8a017a39
KuroiRyu1 <minhphat1612@gmail.com>
2023-09-27 10:15:08 +0700

    done

--- Function.js
+++ Function.js
@@ -1,11 +1,66 @@
-function Tinhtong(a,b){
+//cau a
+function TinhTong(a,b){
     const Ketqua= a + b
     return Ketqua,
-    console.log(Ketqua)
+    console.log('Tinh Tong:',Ketqua)
 }
-Tinhtong(1,5)
-function Tinhhieu(a,b){
+TinhTong(1,5)
+function TinhHieu(a,b){
     Ketqua= a - b
     return Ketqua,
-    console.log(Ketqua)
+    console.log('Tinh Hieu:',Ketqua)
 }
+TinhHieu(1,5)
+function TinhTich(a,b){
+    Ketqua= a*b
+    return Ketqua,
+    console.log('Tinh Tich:',Ketqua)
+}
+TinhTich(1,5)
+function TinhThuong(a,b){
+    Ketqua=a/b
+    return Ketqua,
+    console.log('Tinh Thuong:',Ketqua)
+}
+TinhThuong(1,5)
+//cau b
+function TrungBinhCong(a,b){
+    Ketqua=(a+b)/2
+    return Ketqua,
+    console.log('Trung Binh Cong:',Ketqua)
+}
+TrungBinhCong(10,20)
+//cau d
+const pi=3.14
+function ChuViHinhTron(D){
+    Ketqua=D*pi
+    return Ketqua,
+    console.log('Chu Vi Hinh Tron:',Ketqua)
+}
+ChuViHinhTron(5)
+function DienTichHinhTron(D){
+    Ketqua=pi*(D/2)*(D/2)
+    return Ketqua,
+    console.log('Dien Tich Hinh Tron:',Ketqua)
+}
+DienTichHinhTron(5)
+//cau e
+function ChuViHCN(dai,rong){
+    Ketqua=(dai+rong)/2
+    return Ketqua,
+    console.log('Chu Vi Hinh Chu Nhat:',Ketqua)
+}
+ChuViHCN(15,10)
+function DienTichHCN(dai,rong){
+    Ketqua=dai*rong
+    return Ketqua,
+    console.log('Dien Tich Hinh Chu Nhat',Ketqua)
+}
+DienTichHCN(15,10)
+//cau f
+function TinhVanToc(S,t){
+    Ketqua=S/t
+    return Ketqua,
+    console.log('Van toc:',Ketqua)
+}
+TinhVanToc(120,130/60)
